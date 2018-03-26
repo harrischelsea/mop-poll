1e4f70381db98681c06c76684ebeb02c1d7479bc
harrischelsea <harix_bazdar@hotmail.com>
2018-03-26 22:47:04 +0200

    fixing routes.

--- app.js
+++ app.js
@@ -12,8 +12,6 @@ var users = require('./routes/users');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -23,13 +21,14 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '/client-web/build')));
 
-app.use(cors());
-app.use(require('./util/auth').checkAuth());
 
+app.use(require('./util/auth').checkAuth());
+app.use(cors());
+
+app.use('/users', users);
 app.use('/', function (req, res) {
     res.sendFile(path.join(__dirname, '/client-web/build', 'index.html'));
 });
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
